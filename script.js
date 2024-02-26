2f322959b9369088985760b19caedb3c28d9e429
João <joaog.alves14@gmail.com>
2024-02-26 08:35:24 -0300

    Fixed the regex

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const userInput = document.getElementById("user-input")
-const checkButton = document.getElementById("check-btn")
-const clearButton = document.getElementById("clear-btn")
-const result = document.getElementById("results-div")
-const phoneRegex = /\(?\d{3}\)?[-\.]? *\d{3}[-\.]? *[-\.]?\d{4}/
-
+const userInput = document.getElementById("user-input");
+const checkButton = document.getElementById("check-btn");
+const clearButton = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+const phoneRegex = /^(1\s?)?(\d{3}|\(\d{3}\))[\-\s]?\d{3}[\-\s]?\d{4}$/;
 clearButton.addEventListener("click", () => {
   result.innerText = "";
-})
+});
 
-checkButton.addEventListener('click', () => {
+checkButton.addEventListener("click", () => {
   if (userInput.value === "") {
-    window.alert("Please provide a phone number")
+    window.alert("Please provide a phone number");
   } else if (phoneRegex.test(userInput.value)) {
-    result.innerText = `Valid US number: ${userInput.value}`
+    result.innerText = `Valid US number: ${userInput.value}`;
   } else {
-    result.innerText = `Invalid US number: ${userInput.value}`
+    result.innerText = `Invalid US number: ${userInput.value}`;
   }
-})
+});
