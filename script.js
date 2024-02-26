97e772fb5575c3490d13df22b704025073091654
João <joaog.alves14@gmail.com>
2024-02-26 08:13:50 -0300

    Added clear button function

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const checkButton = document.getElementById("check-btn")
 const clearButton = document.getElementById("clear-btn")
 const result = document.getElementById("results-div")
 
+clearButton.addEventListener("click", () => {
+  result.innerText = "";
+})
+
+checkButton.addEventListener('click', () => {
+  if (userInput.value === "") {
+    window.alert("Please provide a phone number")
+  } else {
+
+  }
+})
