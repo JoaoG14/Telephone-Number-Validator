f730a2a8cb37d77c527f9cec57e45fe5455edbd6
João <joaog.alves14@gmail.com>
2024-02-26 08:27:26 -0300

    Added number validator function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const userInput = document.getElementById("user-input")
 const checkButton = document.getElementById("check-btn")
 const clearButton = document.getElementById("clear-btn")
 const result = document.getElementById("results-div")
+const phoneRegex = /\(?\d{3}\)?[-\.]? *\d{3}[-\.]? *[-\.]?\d{4}/
 
 clearButton.addEventListener("click", () => {
   result.innerText = "";
@@ -10,7 +11,9 @@ clearButton.addEventListener("click", () => {
 checkButton.addEventListener('click', () => {
   if (userInput.value === "") {
     window.alert("Please provide a phone number")
+  } else if (phoneRegex.test(userInput.value)) {
+    result.innerText = `Valid US number: ${userInput.value}`
   } else {
-
+    result.innerText = `Invalid US number: ${userInput.value}`
   }
 })
